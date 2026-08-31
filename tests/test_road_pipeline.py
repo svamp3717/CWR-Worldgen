733224f5680edd5b2bb3af7344f2f9b81399ff28
Simon <svamp3717@users.noreply.github.com>
2026-08-31 20:11:32 +0200

    Track folded emitted seam refinement stage

--- tests/test_road_pipeline.py
+++ tests/test_road_pipeline.py
@@ -23,6 +23,7 @@ def test_cancelled_or_folded_layers_are_not_in_production_pipeline():
         "single_vertex_bend",
         "wrptool_catalogue",
         "stock_obstacles",
+        "emitted_seam_refinement",
     }
     assert retired.isdisjoint(ROAD_PIPELINE_STAGES)
     assert ROAD_PIPELINE_STAGES.index("visual_finish") < ROAD_PIPELINE_STAGES.index(
