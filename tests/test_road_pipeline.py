7d5dc273aad157da3635178a6bfc014b37e834c4
Simon <svamp3717@users.noreply.github.com>
2026-09-01 14:33:12 +0200

    Mark measured and skew junction stages grouped

--- tests/test_road_pipeline.py
+++ tests/test_road_pipeline.py
@@ -28,6 +28,8 @@ def test_cancelled_or_folded_layers_are_not_in_production_pipeline():
         "kodiak_reference",
         "stock_transform",
         "turning_t_fallback",
+        "stock_measured_junction",
+        "stock_skew",
     }
     assert retired.isdisjoint(ROAD_PIPELINE_STAGES)
     assert ROAD_PIPELINE_STAGES.index("visual_finish") < ROAD_PIPELINE_STAGES.index(
