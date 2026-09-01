eddcdc518c600556293633e8c5ae9a5dcd3ae674
Simon <svamp3717@users.noreply.github.com>
2026-09-01 15:32:39 +0200

    Mark candidate selector stage as folded

--- tests/test_road_pipeline.py
+++ tests/test_road_pipeline.py
@@ -32,6 +32,7 @@ def test_cancelled_or_folded_layers_are_not_in_production_pipeline():
         "stock_skew",
         "stock_curve_preservation",
         "sharp_exact",
+        "candidate_enforcement",
     }
     assert retired.isdisjoint(ROAD_PIPELINE_STAGES)
     assert ROAD_PIPELINE_STAGES.index("visual_finish") < ROAD_PIPELINE_STAGES.index(
