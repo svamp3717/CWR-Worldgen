9df379d59043fc5948ea1794b1f06c9df5c4eaaa
Simon <svamp3717@users.noreply.github.com>
2026-09-20 14:49:29 +0200

    Use clean Eden grass beneath shared overlays

--- src/cwr_worldgen/single_runway_background_policy.py
+++ src/cwr_worldgen/single_runway_background_policy.py
@@ -22,7 +22,7 @@ from typing import Iterable, Sequence
 
 STOCK_RUNWAY_BACKGROUND_TEXTURES: dict[str, str] = {
     "nogova": r"o\t1.paa",
-    "everon": r"Eden\zbh.paa",
+    "everon": r"Eden\z.paa",
     "malden": r"abel\tt.paa",
     "desert": r"o\ps.paa",
 }
